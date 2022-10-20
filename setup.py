f74246dfb00d5fee153e4e2b06d61ae0d2e50b3a
ollelindgren <lindgrenolle@live.se>
2022-10-20 20:22:21 +0200

    Add requirements.txt and install_requires in setup

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@ from pathlib import Path
 
 import setuptools
 
+with open(Path(__file__).parent / "requirements.txt", encoding="utf-8") as stream:
+    requirements = list(filter(bool, (x.strip() for x in stream.readlines())))
+
 setuptools.setup(
     name="pyrefact",
     version="1",
     description="Automatic python refactoring",
     author="Olle Lindgren",
     author_email="olle.ln@outlook.com",
-    packages=setuptools.find_packages(str(Path(__file__).parent)),
+    packages=["pyrefact"],
+    install_requires=requirements,
 )
