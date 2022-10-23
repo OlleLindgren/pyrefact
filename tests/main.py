f53e89d8c0f961494e4fc35998728dde43c61cce
ollelindgren <lindgrenolle@live.se>
2022-10-23 23:30:32 +0200

    Specify cwd in subprocess calls in tests/main.py
    I'm hoping this will solve a problem with some path issues in the
    github CI

--- tests/main.py
+++ tests/main.py
@@ -4,10 +4,15 @@ from pathlib import Path
 
 
 def main() -> int:
+    """Run all scripts in the pyrefact/tests folder.
+
+    Returns:
+        int: Always returns 0. If unsuccessful, raises exceptions.
+    """
     for filename in Path(__file__).parent.rglob("*.py"):
         if str(filename.absolute()) != __file__:
             print(f"Testing {filename.name}...")
-            subprocess.check_call([sys.executable, filename])
+            subprocess.check_call([sys.executable, filename], cwd=Path(__file__).parents[1])
             print("PASSED")
 
     print("PASSED")
