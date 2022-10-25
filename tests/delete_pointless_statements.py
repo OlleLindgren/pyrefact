fac28d066cc746b42504430206f79c164f5edafb
ollelindgren <lindgrenolle@live.se>
2022-10-25 21:51:33 +0200

    Add more tests for delete_pointless_statements
    Tests are added for code with module docstrings, with and without a
    shebang.

--- tests/delete_pointless_statements.py
+++ tests/delete_pointless_statements.py
@@ -52,10 +52,29 @@ def f():
 """
 
 
+SHEBANG = """#!/usr/bin/env python3
+
+
+"""
+
+
+REGULAR_MODULE_DOCSTRING = '''"""A normal module docstring"""
+
+def f() -> int:
+    return 0
+
+import sys
+if __name__ == "__main__":
+    sys.exit(f())
+'''
+
+
 def main() -> int:
 
     got = delete_pointless_statements(CODE)
     assert got == EXPECTED, "\n".join(("Wrong result: (got, expected)", got, EXPECTED))
+    assert SHEBANG == delete_pointless_statements(SHEBANG)
+    assert REGULAR_MODULE_DOCSTRING == delete_pointless_statements(REGULAR_MODULE_DOCSTRING)
 
     return 0
 
