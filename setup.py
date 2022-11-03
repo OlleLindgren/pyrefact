1b5a6370856862b35c519f1b0fa5e2e72d8a9d9b
ollelindgren <lindgrenolle@live.se>
2022-11-03 21:13:29 +0100

    Add url to setup.py, uplift version to 11

--- setup.py
+++ setup.py
@@ -9,10 +9,11 @@ if tuple(sys.version_info) < (3, 9):
 
 setuptools.setup(
     name="pyrefact",
-    version="10",
+    version="11",
     description="Automatic python refactoring",
     author="Olle Lindgren",
     author_email="olle.ln@outlook.com",
     packages=["pyrefact"],
     install_requires=["black>=22.1.0", "pylint>=2.12.2", "rmspace>=6"],
+    url="https://github.com/OlleLindgren/pyrefact",
 )
