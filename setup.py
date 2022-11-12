738b36462f9927328992c872a17e1de440564c25
ollelindgren <lindgrenolle@live.se>
2022-11-12 10:15:25 +0100

    Bump version to 16

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if tuple(sys.version_info) < (3, 9):
 
 setuptools.setup(
     name="pyrefact",
-    version="15",
+    version="16",
     description="Automatic python refactoring",
     author="Olle Lindgren",
     author_email="olle.ln@outlook.com",
