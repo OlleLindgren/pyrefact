329e31267e548bc42cfadf796b378786e18a53d0
ollelindgren <lindgrenolle@live.se>
2022-11-13 11:07:19 +0100

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 """Installer for pyrefact"""
 import sys
 import warnings
+from pathlib import Path
 
 import setuptools
 
 if tuple(sys.version_info) < (3, 9):
     warnings.warn("Pyrefact is not tested with python < 3.9, and may not work.")
+
+with open(Path(__file__).parent / "README.md", encoding="utf-8") as stream:
+    LONG_DESCRIPTION = stream.read()
 
 setuptools.setup(
     name="pyrefact",
@@ -16,4 +20,5 @@ setuptools.setup(
     packages=["pyrefact"],
     install_requires=["black>=22.1.0", "isort==5.10.1", "rmspace>=6"],
     url="https://github.com/OlleLindgren/pyrefact",
+    long_description=LONG_DESCRIPTION,
 )
