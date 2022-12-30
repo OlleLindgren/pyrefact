0ec697cdbe91296f1c02b24c230c838c8361b233
ollelindgren <lindgrenolle@live.se>
2022-12-31 00:06:29 +0100

    Fix format_code tests running twice
    Tests accidentally run twice, which is unnecessary.

--- tests/integration/test_format_code.py
+++ tests/integration/test_format_code.py
@@ -16,11 +16,6 @@ def main() -> int:
         if not testing_infra.check_fixes_equal(processed_content, expected_abstraction):
             return 1
 
-    for content, expected_abstraction in INTEGRATION_TEST_CASES:
-        processed_content = pyrefact.format_code(content)
-        if not testing_infra.check_fixes_equal(processed_content, expected_abstraction):
-            return 1
-
     return 0
 
 
