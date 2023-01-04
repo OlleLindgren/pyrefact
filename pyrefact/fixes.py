9eb0e9d08cf50ef47ea143e2fd20222bb76bfc2d
ollelindgren <lindgrenolle@live.se>
2023-01-04 18:31:48 +0100

    Fix BinOp comparison bug

--- pyrefact/fixes.py
+++ pyrefact/fixes.py
@@ -1103,12 +1103,12 @@ def _negate_condition(node: ast.AST) -> ast.AST:
             left=node.left, ops=[opposite_operator_type()], comparators=node.comparators
         )
 
-    if isinstance(node, ast.BinOp) and isinstance(node.operand, ast.And):
+    if isinstance(node, ast.BinOp) and isinstance(node.op, ast.And):
         return ast.BinOp(
             left=_negate_condition(node.left), right=_negate_condition(node.right), op=ast.Or()
         )
 
-    if isinstance(node, ast.BinOp) and isinstance(node.operand, ast.Or):
+    if isinstance(node, ast.BinOp) and isinstance(node.op, ast.Or):
         return ast.BinOp(
             left=_negate_condition(node.left), right=_negate_condition(node.right), op=ast.And()
         )
