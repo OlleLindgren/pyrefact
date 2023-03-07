cd04174e661533974a299a3cc73a5666b5301894
ollelindgren <lindgrenolle@live.se>
2023-03-07 23:33:47 +0100

    Add test formatting numpy/core
    A test is added where pyrefact formats all python files in numpy/core.

---
+++ tests/numpy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Clone the numpy git repo and try to rewrite all python files in it
+# TODO fix the commit hash but keep shallow commit somehow
+
+mkdir -p ~/.cache/pyrefact/tests
+cd ~/.cache/pyrefact/tests
+rm -rf numpy
+git clone --depth=1 git@github.com:numpy/numpy.git
+cd numpy
+pyrefact -sv $(git ls-files | grep 'numpy\/core\/\w*\.py$')
