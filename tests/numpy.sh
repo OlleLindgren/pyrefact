565db2416405a508fc700946d11671710e9aaccc
ollelindgren <lindgrenolle@live.se>
2023-03-07 23:36:04 +0100

    Clone numpy over https instead of ssh
    Fingers crossed this works better in the ci workflow

--- tests/numpy.sh
+++ tests/numpy.sh
@@ -5,6 +5,6 @@
 mkdir -p ~/.cache/pyrefact/tests
 cd ~/.cache/pyrefact/tests
 rm -rf numpy
-git clone --depth=1 git@github.com:numpy/numpy.git
+git clone --depth=1 https://github.com/numpy/numpy.git
 cd numpy
 pyrefact -sv $(git ls-files | grep 'numpy\/core\/\w*\.py$')
